8c8a03e4040902f67ee3d78566605b55e4dab8dc
Petter Karkea <petter@karkea.se>
2019-04-05 16:04:59 +0200

    Updated misson params to x15 resource miltiplier

--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -98,9 +98,9 @@ class Params
 	};
 	class ResourcesMultiplier {
 		title = $STR_PARAMS_RESOURCESMULTIPLIER;
-		values[] = { 0, 1, 2, 3, 4, 5, 6, 7 };
-		texts[] = { "x0.25", "x0.5", "x0.75", "x1", "x1.25","x1.5","x2","x3" };
-		default = 7;
+		values[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+		texts[] = { "x0.25", "x0.5", "x0.75", "x1", "x1.25","x1.5","x2","x3","x15" };
+		default = 8;
 	};
 	class Spacer1 {
 		title = "";
